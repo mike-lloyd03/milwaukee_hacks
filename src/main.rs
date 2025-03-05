d937e3a72ae276853409a32dde43bb6343f0b0d6
Mike Lloyd <mike.lloyd03@pm.me>
2025-03-05 14:09:01 -0800

    Allow multiple cart sizes
    Also fix multiple required products

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,54 @@ use types::*;
 
 use itertools::Itertools;
 
+// The most amount of items in a cart
+const MAX_CART_SIZE: usize = 5;
+// The minimum total cost of the cart
+const MIN_CART_TOTAL: f32 = 1000.0;
+// List of products that must be in the cart (should be less than MAX_CART_SIZE)
+const REQUIRED_PRODUCTS: &[&str] = &["Track Saw", "Jig Saw"];
+
 fn main() {
-    let cart_size = 4;
-    let min_amount = 1000.0;
+    if !(2..=7).contains(&MAX_CART_SIZE) {
+        eprintln!("cart_size must be 2 < x <= 7");
+        return;
+    }
 
-    let toml_string = fs::read_to_string("products.toml").expect("Failed to read file");
-    let products_file: ProductsFile = toml::from_str(&toml_string).expect("Failed to parse TOML");
+    let products = load_products("products.toml");
 
-    let required_products = ["Track Saw"].map(|s| s.to_string());
+    let carts = get_cart_options(&products, MAX_CART_SIZE, MIN_CART_TOTAL, REQUIRED_PRODUCTS);
 
-    let combinations: Vec<Vec<Product>> = products_file
-        .products
+    println!(
+        "Cheapest carts with {:?} over ${}:\n",
+        REQUIRED_PRODUCTS, MIN_CART_TOTAL
+    );
+    for (i, cart) in carts.iter().take(3).enumerate() {
+        println! {"Option {}:", i+1}
+        println!("{cart}");
+        println!("------------\n")
+    }
+}
+
+fn get_cart_options<'a>(
+    products: &'a [Product],
+    max_cart_size: usize,
+    min_total: f32,
+    required_products: &'a [&str],
+) -> Vec<Cart<'a>> {
+    let mut combinations: Vec<Vec<&Product>> = vec![];
+
+    for i in 2..max_cart_size {
+        combinations.append(&mut products.iter().combinations_with_replacement(i).collect())
+    }
+
+    combinations = combinations
         .into_iter()
-        .combinations_with_replacement(cart_size)
-        .filter(|c| c.iter().any(|p| required_products.contains(&p.name)))
-        .filter(|c| c.iter().map(|c| c.price).sum::<f32>() > min_amount)
+        .filter(|c| {
+            required_products
+                .iter()
+                .all(|p| c.iter().any(|c| &c.name == p))
+        })
+        .filter(|c| c.iter().map(|c| c.price).sum::<f32>() >= min_total)
         .collect();
 
     let mut carts: Vec<Cart> = combinations
@@ -32,13 +65,11 @@ fn main() {
 
     carts.sort_by(|a, b| a.total.partial_cmp(&b.total).unwrap());
 
-    println!(
-        "Cheapest carts with {:?} over ${}:\n",
-        required_products, min_amount
-    );
-    for (i, cart) in carts.iter().take(3).enumerate() {
-        println! {"Option {}:", i+1}
-        println!("{cart}");
-        println!("------------\n")
-    }
+    carts
+}
+
+fn load_products(filename: &str) -> Vec<Product> {
+    let toml_string = fs::read_to_string(filename).expect("Failed to read file");
+    let products_file: ProductsFile = toml::from_str(&toml_string).expect("Failed to parse TOML");
+    products_file.products
 }
--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,7 @@
 use std::fmt::Display;
 
 use serde::Deserialize;
-#[derive(Clone, Debug, PartialEq, PartialOrd, Deserialize)]
+#[derive(Debug, PartialEq, PartialOrd, Deserialize)]
 pub struct Product {
     pub name: String,
     pub price: f32,
@@ -13,12 +13,12 @@ impl Display for Product {
     }
 }
 
-pub struct Cart {
-    pub items: Vec<Product>,
+pub struct Cart<'a> {
+    pub items: Vec<&'a Product>,
     pub total: f32,
 }
 
-impl Display for Cart {
+impl Display for Cart<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for item in &self.items {
             writeln!(f, "{}", item)?;
