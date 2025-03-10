49130237ab6c9817f2015d685f654f84d3e5b05e
Mike Lloyd <mike.lloyd03@pm.me>
2025-03-09 23:34:46 -0700

    Basic styling

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -20,51 +20,48 @@
 	}
 </script>
 
-<h1>Milwaukee Hacks</h1>
-<div>
-	<div>cartSize = {cartSize}</div>
-	<div>minTotal = {minTotal}</div>
-	<div>requiredProducts = {JSON.stringify(requiredProducts)}</div>
-</div>
-<div class="flex">
-	<div class="flex flex-col">
-		<label>
-			Max Cart Size
-			<input type="number" name="cartSize" bind:value={cartSize} />
-		</label>
+<div class="text-center">
+	<h1 class="text-lg font-bold text-red-700">Milwaukee Hacks</h1>
+	<div class="flex justify-center">
+		<div class="flex flex-col">
+			<label>
+				Max Cart Size
+				<input type="number" name="cartSize" bind:value={cartSize} />
+			</label>
 
-		<label>
-			Minimum Cart Total
-			<input type="number" name="minTotal" bind:value={minTotal} />
-		</label>
-		<div class="flex h-64 flex-col overflow-y-auto">
-			{#each products as product (product.name)}
-				<label>
-					<input
-						type="checkbox"
-						name="requiredProduct"
-						value={product.name}
-						bind:group={requiredProducts}
-					/>
-					{product.name}
-				</label>
-			{/each}
+			<label>
+				Minimum Cart Total
+				<input type="number" name="minTotal" bind:value={minTotal} />
+			</label>
+			<div class="flex h-64 flex-col overflow-y-auto">
+				{#each products as product (product.name)}
+					<label>
+						<input
+							type="checkbox"
+							name="requiredProduct"
+							value={product.name}
+							bind:group={requiredProducts}
+						/>
+						{product.name}
+					</label>
+				{/each}
+			</div>
+			<button onclick={getCarts}>Calculate</button>
 		</div>
-		<button onclick={getCarts}>Calculate</button>
-	</div>
 
-	{#if carts.length > 0}
 		<div>
 			<h3>Best Carts:</h3>
-			{#each carts as cart, i (cart)}
-				<div>
-					<h4>Option {i + 1}</h4>
-					{#each cart.items as item (item)}
-						<p>{item.name} {item.price}</p>
-					{/each}
-					<p>Total: {cart.total}</p>
-				</div>
-			{/each}
+			{#if carts.length > 0}
+				{#each carts as cart, i (cart)}
+					<div>
+						<h4>Option {i + 1}</h4>
+						{#each cart.items as item (item)}
+							<p>{item.name} {item.price}</p>
+						{/each}
+						<p>Total: {cart.total}</p>
+					</div>
+				{/each}
+			{/if}
 		</div>
-	{/if}
+	</div>
 </div>
