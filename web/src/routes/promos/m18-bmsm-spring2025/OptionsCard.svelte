2b86d532ec6ba2b63d940dfff0e9386bb7ec4a76
Mike Lloyd <mike.lloyd03@pm.me>
2025-03-19 22:00:06 -0700

    Rounded imgs

--- web/src/routes/promos/m18-bmsm-spring2025/OptionsCard.svelte
+++ web/src/routes/promos/m18-bmsm-spring2025/OptionsCard.svelte
@@ -85,7 +85,11 @@
 								/>
 								<div class="flex w-full items-center justify-between">
 									<div class="flex items-center">
-										<img src={product.image_primary_url.replace('<SIZE>', '65')} alt="tool" />
+										<img
+											src={product.image_primary_url.replace('<SIZE>', '65')}
+											alt="tool"
+											class="rounded-md"
+										/>
 										<a
 											href={`https://www.homedepot.com${product.canonical_url}`}
 											class="mx-2 hover:underline"
