555309ba7aea7c84e9069f2a0b959108c6bd599b
Mike Lloyd <mike.lloyd03@pm.me>
2025-03-19 23:32:01 -0700

    Add contact page

---
+++ web/src/routes/contact/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { P } from 'flowbite-svelte';
+</script>
+
+<div class="space-y-4">
+	<P>
+		I wrote this tool to make it easier to find the best deals on certain Milwaukee tool promotions
+		at Home Depot. I turned it into a web app to share with the community so others can share in the
+		joy of new tools. I also learned some cool stuff about Rust WASM with Typescript.
+	</P>
+
+	<P>
+		My hope is that Milwaukee/TTI and Home Depot won't have a problem with this as, at least in my
+		experience, it allows people to buy tools they otherwise wouldn't buy. I've wanted a track saw
+		for a long time but simply can't justify $400 for one. In this situation, Milwaukee/Home Depot
+		make zero dollars off me. But with the hack, I just bought one for $270. I get a tool that I
+		want and the manufacturer gets money out of my pocket. It's a win for everyone.
+	</P>
+
+	<P>
+		If you have questions about this tool, you can open a <a
+			class="text-blue-500 underline"
+			href="https://github.com/mike-lloyd03/milwaukee_hacks/discussions"
+			target="_blank"
+			>discussion on GitHub
+		</a>.
+	</P>
+
+	<P>If you have code changes, PRs are very welcome.</P>
+
+	<P>
+		If you work for Milwaukee and want to send me free tools that would be great. If instead you
+		want me to shut this down, that would make me sad.
+	</P>
+</div>
