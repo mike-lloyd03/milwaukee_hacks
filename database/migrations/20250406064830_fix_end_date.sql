0a408d51b3afdb888b9427c492d4302cd49e7767
Mike Lloyd <mike.lloyd03@pm.me>
2025-04-05 23:50:41 -0700

    Fix spelling of promotion.end_date in db

---
+++ database/migrations/20250406064830_fix_end_date.sql
@@ -0,0 +1,30 @@
+CREATE TABLE temp_promotions (
+    promotion_id TEXT PRIMARY KEY,
+    experience_tag TEXT,
+    sub_experience_tag TEXT,
+    long_description TEXT,
+    short_description TEXT,
+    start_date TEXT,
+    end_date TEXT,
+    item_group TEXT,
+    categories JSON,
+    item_ids JSON ,
+    eligibility_min_purchase_amount REAL,
+    eligibility_min_purchase_quantity REAL,
+    max_allowed_reward_amount REAL,
+    max_purchase_quantity REAL,
+    min_purchase_amount REAL,
+    min_purchase_quantity REAL,
+    reward_amount_per_item REAL,
+    reward_amount_per_order REAL,
+    reward_fixed_price REAL,
+    reward_percent REAL
+);
+
+INSERT INTO temp_promotions (promotion_id, experience_tag, sub_experience_tag, long_description, short_description, start_date, end_date, item_group, categories, item_ids, eligibility_min_purchase_amount, eligibility_min_purchase_quantity, max_allowed_reward_amount, max_purchase_quantity, min_purchase_amount, min_purchase_quantity, reward_amount_per_item, reward_amount_per_order, reward_fixed_price, reward_percent)
+SELECT promotion_id, experience_tag, sub_experience_tag, long_description, short_description, start_date, end_data, item_group, categories, item_ids, eligibility_min_purchase_amount, eligibility_min_purchase_quantity, max_allowed_reward_amount, max_purchase_quantity, min_purchase_amount, min_purchase_quantity, reward_amount_per_item, reward_amount_per_order, reward_fixed_price, reward_percent
+FROM promotions;
+
+DROP TABLE promotions;
+
+ALTER TABLE temp_promotions RENAME TO promotions;
