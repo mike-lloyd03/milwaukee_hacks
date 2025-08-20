309808b584ccbec814b164be9b5d0c3e0869be13
Mike Lloyd <mike.lloyd03@pm.me>
2025-08-19 22:55:20 -0700

    Get promos async

--- database/src/main.rs
+++ database/src/main.rs
@@ -1,15 +1,16 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use anyhow::Result;
 use database::requests::{self, get_product, Brand, Department, SearchParams};
-use database::types::{ProductDB, Promotion, PromotionDB};
+use database::types::{Product, ProductDB, Promotion, PromotionDB};
 use database::{config, now_timestamp};
 use futures::future::join_all;
 use sqlx::{sqlite::SqliteConnectOptions, SqlitePool};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let config = config::Config::load("config.toml")?;
+    let load = config::Config::load("config.toml")?;
+    let config = load;
     let options = SqliteConnectOptions::new()
         .filename(&config.db_path)
         .create_if_missing(true);
@@ -35,61 +36,94 @@ async fn main() -> Result<()> {
 
     let now = now_timestamp();
 
-    let mut promos: Vec<Promotion> = Vec::new();
     let products = requests::get_products(search_params)?;
 
-    for product in &products {
-        let product_promos: Vec<String> = product
-            .pricing
-            .conditional_promotions
-            .iter()
-            .filter_map(|p| p.promotion_id.map(|v| v.to_string()))
-            .collect();
+    let promos = get_all_promos(&products).await;
 
-        let existing_promo_ids: Vec<String> =
-            promos.iter().map(|p| p.promotion_id.clone()).collect();
+    let additional_products = get_additional_products(&products, &promos).await?;
 
-        if !existing_promo_ids
-            .iter()
-            .any(|existing_promo| product_promos.contains(existing_promo))
-        {
-            match requests::get_promos_for_product(&product.id) {
-                Ok(p) => {
-                    for promo in p {
-                        println!(
-                            "Got promo {}: {}",
-                            &promo.promotion_id,
-                            promo
-                                .description
-                                .long_desc
-                                .clone()
-                                .unwrap_or(promo.description.short_desc.clone())
-                        );
-
-                        promos.push(promo);
-                    }
-                }
-                Err(e) => {
-                    let err_msg = e.to_string();
-                    let skip_errors = ["Promotion not found", "No promotions for product"];
-
-                    if skip_errors.iter().any(|e| err_msg.contains(e)) {
-                        println!("Warning: {e}");
-                        continue;
-                    }
-                    return Err(e);
-                }
-            };
-        }
-    }
     println!("Got {} products", products.len());
     println!("Got {} promos", promos.len());
+    println!("Got {} additional products", additional_products.len());
+
+    let mut tx = pool.begin().await?;
 
     for product in products {
         let product_db: ProductDB = product.into();
-        product_db.create(&pool).await?;
+        product_db.create(&mut *tx).await?;
     }
 
+    for promo in promos {
+        let promo_db: PromotionDB = promo.into();
+        promo_db.create(&mut *tx).await?;
+    }
+
+    for product in additional_products {
+        let product_db: ProductDB = product.into();
+        product_db.create(&mut *tx).await?;
+    }
+
+    tx.commit().await?;
+
+    ProductDB::delete_all_before(&pool, now).await?;
+    PromotionDB::delete_all_before(&pool, now).await?;
+
+    Ok(())
+}
+
+async fn get_all_promos(products: &Vec<Product>) -> Vec<Promotion> {
+    // Map of promo_id: product item_id that returns that promo
+    let mut expected_promo_ids: HashMap<u32, String> = HashMap::new();
+
+    for product in products {
+        product
+            .pricing
+            .conditional_promotions
+            .iter()
+            .for_each(|promo| {
+                if let Some(promo_id) = promo.promotion_id {
+                    expected_promo_ids
+                        .entry(promo_id)
+                        .or_insert(product.id.clone());
+                }
+            });
+    }
+
+    println!("Expecting {} promos", expected_promo_ids.len());
+
+    let futures = expected_promo_ids.values().map(|item_id| {
+        let id_clone = item_id.clone();
+
+        tokio::task::spawn_blocking(move || {
+            let promos = requests::get_promos_for_product(&id_clone).unwrap();
+            promos.iter().for_each(|promo| {
+                println!(
+                    "Got promo {}: {}",
+                    &promo.promotion_id,
+                    promo
+                        .description
+                        .long_desc
+                        .clone()
+                        .unwrap_or(promo.description.short_desc.clone())
+                );
+            });
+            promos
+        })
+    });
+
+    let results = join_all(futures).await;
+
+    results
+        .into_iter()
+        .filter_map(Result::ok)
+        .flatten()
+        .collect()
+}
+
+async fn get_additional_products(
+    products: &[Product],
+    promos: &[Promotion],
+) -> Result<Vec<Product>> {
     let mut items_to_fetch: HashSet<String> = HashSet::new();
 
     for promo in promos {
@@ -99,14 +133,11 @@ async fn main() -> Result<()> {
         // referencing
         for ec in promo.eligibility_criteria.iter() {
             for item_id in ec.item_ids.iter() {
-                if !ProductDB::check_exists(&pool, item_id).await? {
+                if !products.iter().any(|p| p.id == *item_id) {
                     items_to_fetch.insert(item_id.to_string());
-                };
+                }
             }
         }
-
-        let promo_db: PromotionDB = promo.into();
-        promo_db.create(&pool).await?;
     }
 
     let futures = items_to_fetch.iter().map(|item_id| {
@@ -114,27 +145,11 @@ async fn main() -> Result<()> {
 
         tokio::task::spawn_blocking(move || {
             println!("Promo item {id_clone} missing from DB. Fetching...");
-            let product = get_product(&id_clone).unwrap();
-            let product_db: ProductDB = product.into();
-            product_db
+            get_product(&id_clone).unwrap()
         })
     });
 
     let results = join_all(futures).await;
 
-    let new_products: Vec<ProductDB> = results.into_iter().filter_map(Result::ok).collect();
-
-    let mut tx = pool.begin().await?;
-
-    println!("Adding {} products...", new_products.len());
-    for product in new_products {
-        product.create(&mut *tx).await?;
-    }
-
-    tx.commit().await?;
-
-    ProductDB::delete_all_before(&pool, now).await?;
-    PromotionDB::delete_all_before(&pool, now).await?;
-
-    Ok(())
+    Ok(results.into_iter().filter_map(Result::ok).collect())
 }
